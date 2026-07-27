ee61f10c23656d56d46f5dd7cd3bd597dbbf9c23
boby-technicise <boby.technicise@gmail.com>
2026-07-27 19:26:39 +0530

    layout modified

--- attendance_system/urls.py
+++ attendance_system/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('attendance.urls')),
 ]
